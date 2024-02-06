4dc235a17cb3020f6e8ef9cf5265d91b9bebdb4f
AlineArj <alinearj@usp.br>
2024-02-05 21:57:22 -0300

    Exercicio 49, primeiro do ano

--- Seção 4 - Variáveis e Tipos de Dados/ex_49.py
+++ Seção 4 - Variáveis e Tipos de Dados/ex_49.py
@@ -6,10 +6,24 @@ while resp_num == 0:
     try:
         h_i = int(input("Digite a hora: "))
         m_i = int(input("Digite os minutos: "))
-        s_i = int(input("Digite os segundos:"))
-        duracao = int(input("\nDigite a duração da experiência em segundos: "))
+        s_i = int(input("Digite os segundos: "))
+        d = int(input("\nDigite a duração da experiência em segundos: "))
 
-            
+        if d > 3599:
+            h_f = h_i + int(d / 3600)
+            d %= 3600
+        else:
+            h_f = h_i
+
+        if d > 59:
+            m_f = m_i + int(d / 60)
+            d %= 60
+        else:
+            m_f = m_i
+        
+        s_f = s_i + d
+
+        print(f"A experiência irá acabar as {h_f:.0f}:{m_f:.0f}:{s_f:.0f}")
         resp_num = 1
 
     except:
